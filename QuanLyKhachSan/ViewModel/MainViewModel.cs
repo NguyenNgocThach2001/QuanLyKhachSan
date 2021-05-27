a4885ac57789274f84f161ca09f581c290551b09
Thach <19520265@gm.uit.edu.vn>
2021-05-27 22:03:22 +0700

    First commit

---
+++ QuanLyKhachSan/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKhachSan.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public static bool isLoaded = false;
+        public MainViewModel()
+        {
+            if (!isLoaded)
+            {
+                isLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
