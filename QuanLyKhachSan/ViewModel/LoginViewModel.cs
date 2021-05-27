9a81fd7ecee178ff238e1ab43dfa47079a8dfa90
Thach <19520265@gm.uit.edu.vn>
2021-05-28 02:16:24 +0700

    Fix ControlBar resize bug

---
+++ QuanLyKhachSan/ViewModel/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuanLyKhachSan.ViewModel
+{
+    public class LoginViewModel:BaseViewModel
+    {
+        #region commands
+        public ICommand loginDone_btnClick { get; set; }
+        public ICommand loginClose_btnClick { get; set; }
+        #endregion
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
