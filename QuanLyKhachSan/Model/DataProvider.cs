f4622be9cf6144ec6e8b1af945408fde99f2ff12
Thach <19520265@gm.uit.edu.vn>
2021-06-01 08:42:08 +0700

    Add database and login

---
+++ QuanLyKhachSan/Model/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.Model
+{
+    public class DataProvider
+    {
+        public QuanLyKhachSanEntities1 db { get; set; }
+        private static DataProvider _Ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_Ins == null)
+                    _Ins = new DataProvider();
+                return _Ins;
+            }
+            set
+            {
+                _Ins = value;
+            }
+        }
+        public DataProvider()
+        {
+            db = new QuanLyKhachSanEntities1();
+        }
+    }
+}
--- QuanLyKhachSan/ViewModel/LoginViewModel.cs
+++ QuanLyKhachSan/ViewModel/LoginViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows;
 using System.Windows.Controls;
 using System.Security.Cryptography;
+using QuanLyKhachSan.Model;
 
 namespace QuanLyKhachSan.ViewModel
 {
@@ -17,17 +18,67 @@ namespace QuanLyKhachSan.ViewModel
         public ICommand loginClose_btnClick { get; set; }
         #endregion
         public bool IsLogin { get; set; }
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangeCommand { get; set; }
+        private string _UserName;
+        public string UserName
+        {
+            get => _UserName;
+            set { _UserName = value; OnPropertyChanged(); }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get => _Password;
+            set { _Password = value; OnPropertyChanged(); }
+        }
+
         public LoginViewModel()
         {
-            loginDone_btnClick = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            IsLogin = false;
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { Login(p); });
+            PasswordChangeCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+        }
+        void Login(Window p)
+        {
+            if (p == null) return;
+            if (UserName == null) return;
+            if (Password == null) return;
+            string passEncode = MD5Hash(Base64Encode(Password));
+            var init = DataProvider.Ins.db.Accounts.ToList().Count();
+            if (init == 0)
             {
-                var HashCodePassword = MD5Hash("a");
+                Account ac = new Account();
+                ac.UserName = UserName;
+                ac.Password = MD5Hash(Base64Encode(Password));
+                DataProvider.Ins.db.Accounts.Add(ac);
+                DataProvider.Ins.db.SaveChanges();
                 IsLogin = true;
                 p.Close();
             }
-            );
+            else
+            {
+                var check = DataProvider.Ins.db.Accounts.Where(x => x.UserName == UserName && x.Password == passEncode).Count();
+                if (check > 0)
+                {
+                    IsLogin = true;
+                    p.Close();
+                }
+                else
+                {
+                    IsLogin = false;
+                    MessageBox.Show("Sai tài khoản hoặc mật khẩu");
+                }
+            }
+
         }
-        public static string MD5Hash(string text)
+        public string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public string MD5Hash(string text)
         {
             MD5 md5 = new MD5CryptoServiceProvider();
 
