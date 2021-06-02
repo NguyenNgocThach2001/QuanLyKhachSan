6326bd7d3014830a49bbe782d0fa52f96bb51df7
Thach <19520265@gm.uit.edu.vn>
2021-06-02 07:23:50 +0700

    Add room status list

--- QuanLyKhachSan/ViewModel/MainViewModel.cs
+++ QuanLyKhachSan/ViewModel/MainViewModel.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using QuanLyKhachSan.Model;
+using System.Windows.Media;
 
 namespace QuanLyKhachSan.ViewModel
 {
@@ -14,6 +17,15 @@ namespace QuanLyKhachSan.ViewModel
         #region commands
         public ICommand window_IsLoaded { get; set; }
         #endregion
+        private ObservableCollection<RoomModel> _RoomList;
+        public ObservableCollection<RoomModel> RoomList
+        {
+            get => _RoomList;
+            set
+            {
+                _RoomList = value; OnPropertyChanged();
+            }
+        }
         public static bool isLoaded = false;
         #region commands
         public ICommand AccountCommand { get; set; }
@@ -37,6 +49,7 @@ namespace QuanLyKhachSan.ViewModel
                     if (loginVM.IsLogin)
                     {
                         p.Show();
+                        LoadRoomList();
                     }
                     else
                     {
@@ -66,6 +79,26 @@ namespace QuanLyKhachSan.ViewModel
                 checkoutWindow.ShowDialog();
             });
         }
-
+        void LoadRoomList()
+        {
+            RoomList = new ObservableCollection<RoomModel>();
+            var roomList = DataProvider.Ins.db.Rooms;
+            foreach(var item in roomList)
+            {
+                var queryRoomType = DataProvider.Ins.db.RoomTypes.FirstOrDefault(x => x.room_type_id == item.room_type_id);
+                RoomModel newRoom = new RoomModel();
+                newRoom.room_name = item.room_name;
+                newRoom.room_price = queryRoomType.price.ToString() + " vnđ";
+                newRoom.room_type = queryRoomType.room_type_name;
+                newRoom.room_status = DataProvider.Ins.db.RoomStatus.FirstOrDefault(x => x.room_status_id == item.room_status_id).room_status_name;
+                if (newRoom.room_status == "Trống")
+                    newRoom.color = new SolidColorBrush(Colors.Green);
+                else
+                {
+                    newRoom.color = new SolidColorBrush(Colors.Red);
+                }
+                RoomList.Add(newRoom);
+            }
+        }
     }
 }
