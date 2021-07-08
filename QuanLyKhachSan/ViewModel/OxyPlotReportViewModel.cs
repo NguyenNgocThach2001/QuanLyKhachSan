09d1aa755c4aebbfc90c6ee055df58ad02bd0758
Thach <19520265@gm.uit.edu.vn>
2021-07-09 02:45:53 +0700

    Add OxyPlot

---
+++ QuanLyKhachSan/ViewModel/OxyPlotReportViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using OxyPlot;
+using OxyPlot.Series;
+using QuanLyKhachSan.Model;
+
+namespace QuanLyKhachSan.ViewModel
+{
+
+    public class OxyPlotReportViewModel : BaseViewModel
+    {
+        public ObservableCollection<OxyPlotReportModel> MyModels = new ObservableCollection<OxyPlotReportModel>();
+        public OxyPlotReportViewModel()
+        {
+            
+        }
+    }
+}
